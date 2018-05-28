58a74c45478f0304d98afa9dfa50839cef7b3b14
durban.zhang <durban.zhang@gmail.com>
2018-05-28 23:17:12 +0800

    How to use UITableView

--- swift_demo/Tabbar3ViewController.swift
+++ swift_demo/Tabbar3ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class Tabbar3ViewController: UIViewController {
-
+class Tabbar3ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var dataArr = NSMutableArray()
+    var tableView: UITableView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,6 +19,20 @@ class Tabbar3ViewController: UIViewController {
         
         self.title = "Tabbar3"
         self.view.backgroundColor = UIColor.yellow
+        
+        // 初始化数据源
+        for i in (0...100) {
+            dataArr.add("row \(i)")
+        }
+        
+        let rect = self.view.bounds;
+        tableView = UITableView(frame: rect, style: UITableViewStyle.plain)
+        tableView!.dataSource = self
+        tableView!.delegate = self
+        self.view.addSubview(tableView!)
+        
+        print(dataArr)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,6 +40,26 @@ class Tabbar3ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataArr.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "cellIdentifier"
+        var cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        if (cell == nil) {
+            cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: cellIdentifier)
+        }
+        
+        let s = dataArr.object(at: indexPath.row) as? String
+        cell?.textLabel?.text = s
+        return cell!
+    }
+    
+    
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("row \(indexPath.row) selected")
+    }
 
     /*
     // MARK: - Navigation
